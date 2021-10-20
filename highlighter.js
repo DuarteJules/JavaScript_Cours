b69ecb973e64ecc1d87f9c577e966861cc7749f3
Lucas <tate.lucas@outlook.fr>
2021-10-20 15:36:15 +0200

    commit

--- highlighter.js
+++ highlighter.js
@@ -1,10 +1,13 @@
-let Lbool = false; 
 
+//Création d'un fonction LHihglight
 function LHihglight(){
     let Lli = document.querySelectorAll('li');
     console.log(Lli); 
+    //création d'une boucle que selectionne chacuns de mes éléments(et mes indexs mais pas important)
+    
     Lli.forEach(function(li, index){
         console.log(li,index)
+        // j'ajoute deux événement à mes éléments
         li.addEventListener('mouseover',colorgreen)
         li.addEventListener('mouseleave',colorNothing)
     });
@@ -12,15 +15,15 @@ function LHihglight(){
 
 }
 
-
+// Je définis les événements de mes éléments 
 function colorgreen(){
-let cases = this
-cases.style.color = 'red'
+    let Lcases = this
+    Lcases.style.color = 'red'
 }
 
 function colorNothing(){
-let cases = this
-cases.style.color = 'black'
+    let Lcases = this
+    Lcases.style.color = 'black'
 }
 
 
