ca5e946a66b4b05d3c9aacbd3540f6c6a261551f
David <david.roquain@edu.esiee-it.fr>
2021-10-20 16:00:08 +0200

    Choix défi 11 (Suppression des console.log)

--- highlighter.js
+++ highlighter.js
@@ -2,11 +2,9 @@
 //Création d'un fonction LHihglight
 function LHihglight(){
     let Lp = document.querySelectorAll('p');
-    console.log(Lp); 
     //création d'une boucle que selectionne chacuns de mes éléments(et mes indexs mais pas important)
     
     Lp.forEach(function(p, index){
-        console.log(p,index)
         // j'ajoute deux événement à mes éléments
         p.addEventListener('mouseover',colorgreen)
         p.addEventListener('mouseleave',colorNothing)
