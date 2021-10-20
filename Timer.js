539ae65ca6defe7fa62dd957dbaa16855cc3d930
Jules <jules.duarte@edu.esiee-it.fr>
2021-10-20 11:34:59 +0200

    timer finis jeu 1

--- Timer.js
+++ Timer.js
@@ -1,21 +1,21 @@
 //création de la div pour le timer
-let jTimer = document.getElementById('tmp')
-
-//Création de la fonction pour diminuer le temps
-function dimininuerLeTemps(){
-jZoneTimer.innerHTML = temps1
-temps1--
-}
+const jTimer = document.getElementById('tmp')
 
 if (Dbooldef1 == true){
 
     //Création du timer pour le jeu 1
 
-let jZoneTimer = document.createElement('span')
-let temps1 = 120
-setInterval(dimininuerLeTemps, 1000)
-jTimer.appendChild(jZoneTimer)
+let temps = 120
+//Création de la fonction pour diminuer le temps
+setInterval(()=> {
+    let minutes = parseInt(temps / 60,10)
+    let secondes = parseInt(temps % 60,10)
+    
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+    jTimer.innerText = `${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+    console.log('test')
+}, 1000)
 }
-
-
 
