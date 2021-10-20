e3c813b98cd9f9a9b5a84ac51f02a3b9e23a8340
Jules <jules.duarte@edu.esiee-it.fr>
2021-10-20 12:06:57 +0200

    commit

--- Timer.js
+++ Timer.js
@@ -3,27 +3,7 @@ const jTimer = document.getElementById('tmp')
 //création de la variable pour le timer 
 let jtemps = 0
 
-<<<<<<< HEAD
-if (Dbooldef1 == true){
 
-    //Création du timer pour le jeu 1
-
-jtemps = 120
-//Création de la fonction pour diminuer le temps
-setInterval(diminuerLeTemps, 1000)
-}
-
-function diminuerLeTemps (){
-    let minutes = parseInt(jtemps / 60,10)
-    let secondes = parseInt(jtemps % 60,10)
-    
-    minutes = minutes < 10 ? "0" + minutes : minutes
-    secondes = secondes < 10 ? "0" + secondes : secondes
-    jTimer.innerText = `${minutes}:${secondes}`
-    jtemps = jtemps <= 0 ? 0 : jtemps - 1
-    
-}
-=======
 function timer (bool){
     if (bool == true){
 
@@ -45,4 +25,4 @@ function timer (bool){
         
     }  
 }
->>>>>>> main
+
