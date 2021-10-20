a4ae2bcb5f0b472aa0b14ebc8b32d11eff0ffa35
David <david.roquain@edu.esiee-it.fr>
2021-10-20 10:47:37 +0200

    Choix défi 2
    Liste des variables et fonctions :
    Ddefi1 : Retourne le bouton du défi 1
    Ddefi2 : Retourne le bouton du défi 2
    Ddivreg : Retourne l'emplacement du choix des défis
    Ddiv1 : Retourne l'emplacement du nom du défi
    Ddiv2 : Retourne l'emplacement sous le nom du défi
    Ddiv3 : Retourne l'emplacement de la quantité de bonne/mauvaise réponse
    Dbooldef1 : Retourne true si le défi choisit est le 1
    DFunDefi1 : Fonction où placer tout le code du défi 1
    DFunDefi2 : Fonction où placer tout le code du défi 2

--- projetChoixdéfi.js
+++ projetChoixdéfi.js
@@ -2,9 +2,21 @@ let Ddefi1 = document.getElementById('button1')
 Ddefi1.addEventListener('click',DFunDefi1)
 let Ddefi2 = document.getElementById('button2')
 Ddefi2.addEventListener('click',DFunDefi2)
+let Ddivreg = document.getElementById('regles')
+let Ddiv1 = document.getElementById('div1')
+let Ddiv2 = document.getElementById('div2')
+let Ddiv3 = document.getElementById('div3')
 function DFunDefi1(){
-    console.log('Defi1')
+    let Dbooldef1 = true;
+    console.log(Dbooldef1)
+    Ddiv1.innerHTML = `<b>Défi Chrono</b>`
+    Ddiv3.innerHTML = `<p>Bonne réponse/Dbon-Mauvaise réponse/Dmau</p>`
+    Ddivreg.innerHTML = ``
 }
 function DFunDefi2(){
-    console.log('Defi2')
+    let Dbooldef1 = false;
+    console.log(Dbooldef1)
+    Ddiv1.innerHTML = `<b>Défi Incollable</b>`
+    Ddiv3.innerHTML = `<p>Bonne réponse/Dbon-Mauvaise réponse/Dmau</p>`
+    Ddivreg.innerHTML =``
 }
