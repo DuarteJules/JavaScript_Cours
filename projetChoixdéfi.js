7cc9d567c7786930a6e690481fa96c885c0db8b9
David <david.roquain@edu.esiee-it.fr>
2021-10-20 10:29:49 +0200

    Liste des variables et fonctions
    Ddefi1 : Retourne le bouton du défi 1
    Ddefi2 : Retourne le bouton du défi 2
    DFunDefi1 : Fonction où placer tout le code du défi 1
    DFunDefi2 : Fonction où placer tout le code du défi 2

---
+++ projetChoixdéfi.js
@@ -0,0 +1,10 @@
+let Ddefi1 = document.getElementById('button1')
+Ddefi1.addEventListener('click',DFunDefi1)
+let Ddefi2 = document.getElementById('button2')
+Ddefi2.addEventListener('click',DFunDefi2)
+function DFunDefi1(){
+    console.log('Defi1')
+}
+function DFunDefi2(){
+    console.log('Defi2')
+}
